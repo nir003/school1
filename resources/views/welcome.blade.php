2a1d0c15f941aae7463deb02a954aa38a49e282a
nir003 <shnirjhor3029@gmail.com>
2018-07-23 19:50:22 +0600

    chenge welcome blade laravel to Nirjhor

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Nirjhor
                 </div>
 
                 <div class="links">
